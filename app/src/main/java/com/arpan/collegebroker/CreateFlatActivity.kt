9ce29ea879e224183adeadd7181ef94fd6728839
Arpan Srivastava (Ubuntu) <arpansri98@gmail.com>
2018-04-28 01:33:33 +0530

    Removed unnecessary code from CreateFlatActivity.kt

--- app/src/main/java/com/arpan/collegebroker/CreateFlatActivity.kt
+++ app/src/main/java/com/arpan/collegebroker/CreateFlatActivity.kt
@@ -21,59 +21,5 @@ class CreateFlatActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_container)
 
-//        initFragments()
-        println("Lol")
     }
-
-//    private fun initFabs() {
-//        viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
-//            override fun onPageScrollStateChanged(state: Int) {
-//            }
-//
-//            override fun onPageScrolled(current: Int, positionOffset: Float, positionOffsetPixels: Int) {
-//
-//                if (current == 0) { //SellingFragment
-//                    setHorizontalBias(uploadContactFab.id, 0.5f + positionOffset / 4)
-//                    setHorizontalBias(uploadPhotoFab.id, 0.5f - positionOffset / 4)
-//
-//                    mainFab.setOnClickListener { viewPager.setCurrentItem(1, true) }
-//                }
-//
-//                else if (current == 1) {
-//                    mainFab.setOnClickListener { submitCallbackListener!!.submitFlatDetails() }
-//                }
-//            }
-//
-//            override fun onPageSelected(position: Int) {
-//                if (position == 0) {
-//                    uploadPhotoFab.hide()
-//                    uploadContactFab.hide()
-//                }
-//
-//                if (position == 1) {
-//                    uploadPhotoFab.show()
-//                    uploadContactFab.show()
-//                }
-//
-//            }
-//
-//        })
-//    }
-
-    private fun setHorizontalBias(layout: Int, bias: Float) {
-        val constraintSet = ConstraintSet()
-        constraintSet.clone(activity_container)
-
-        constraintSet.apply {
-            setHorizontalBias(layout, bias)
-        }
-
-        constraintSet.applyTo(activity_container)
-    }
-//
-//    private fun initFragments() {
-//
-//        fragments.add(SellingFragment())
-//        fragments.add(CreateFlatFragment())
-//    }
 }
